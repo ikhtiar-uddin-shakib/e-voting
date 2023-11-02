abb401573bb711064d13d4c23b2b0cc38a57139c
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2023-11-02 18:24:06 +0600

    new

--- ducsu_online_voting/urls.py
+++ ducsu_online_voting/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('administrator/', include('administration.urls')),
     path('voting/', include('voting.urls')),
     path('api/', include('api.urls')),
-    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
