8a5064163d84d2cba8b40b52aa07a5fae4e5ab24
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2023-11-02 18:30:05 +0600

    new

--- ducsu_online_voting/settings.py
+++ ducsu_online_voting/settings.py
@@ -130,14 +130,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_URL = 'static/'
+# # STATIC_ROOT = BASE_DIR / 'static'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR/'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR/'static',
+# ]
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
